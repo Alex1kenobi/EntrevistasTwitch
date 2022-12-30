f9708516c68d2dc00601352ecf2191951367f60c
andres-av <andres.av.dev@gmail.com>
2022-12-30 10:26:30 +0000

    added proximas entrevistas

--- src/front/js/pages/interview.js
+++ src/front/js/pages/interview.js
@@ -12,8 +12,22 @@ export const Interview = () => {
           <div className="container profileContainer">
             <div className="box">
               <h3 className="h-dash text-center">Entrevistas</h3>
-              <p className="ms-5">Consulta las entrevistas que hemos hecho hasta el momento</p>
-              <Card2/>
+              <p className="ms-5">
+                Consulta las entrevistas que hemos hecho hasta el momento
+              </p>
+              <Card2 />
+            </div>
+          </div>
+        </section>
+
+        <section className="">
+          <div className="container text-center">
+            <div className="homeSection flex-column">
+              <h2 className="my-5">Próximas Entrevistas</h2>
+
+              <div className="d-flex align-items-center justify-content-center mb-5 flex-wrap">
+                <Card />
+              </div>
             </div>
           </div>
         </section>
